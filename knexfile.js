81360263b9c4afde8d652840495de571058c189a
Eugene <eugenetsai@berkeley.edu>
2017-07-13 19:38:10 -0700

    production database url in knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@
 module.exports = {
     development: {
         client: 'postgresql',
-        connection: 'postgres://localhost/tasks',
+        connection: 'postgres://localhost/tasks'
     },
     production: {
         client: 'postgresql',
-        connection: process.env.DATABASE_URL + '?ssl=true'
+        connection: 'postgres://gewbnqycrjnbbv:c7c6b1c860a6a6b2d526325a702681d04236a9b1e631a8f9c0243c1c997b6a51@ec2-184-73-199-72.compute-1.amazonaws.com:5432/deqol00a7suqjj'
     }
 };
